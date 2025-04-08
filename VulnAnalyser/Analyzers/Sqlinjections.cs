af85559318fdae9f0cb734ef2f0ee08729594ef4
Nils <152011377+VELIKSONNN@users.noreply.github.com>
2025-04-08 13:57:38 +0200

    ajout classe analyzsers

---
+++ VulnAnalyser/Analyzers/Sqlinjections.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VulnAnalyser.Analyzers
+{
+    internal class Sqlinjections
+    {
+      
+
+        static SyntaxNode Creationnoeud()
+        {
+            var code = File.ReadAllText("Test.cs");
+             var syntaxTree = CSharpSyntaxTree.ParseText(code); // permet d'avoir un arbres de toutes la syntax d'un fichiers ( déclaration d'une variable, assignation de valeur etc)
+              var root = syntaxTree.GetRoot(); // SyntaxNode
+            return root;
+        }
+
+        public override void VisitObjectCreationExpression(ObjectCreationExpressionSyntax node)
+        {
+            var typeName = node.Type.ToString();
+            if (typeName.Contains("SqlCommand"))
+            {
+                Console.WriteLine($"⚠️ SqlCommand détecté : {node.GetLocation().GetLineSpan().StartLinePosition}");
+            }
+            base.VisitObjectCreationExpression(node);
+        }
+
+    }
+}
